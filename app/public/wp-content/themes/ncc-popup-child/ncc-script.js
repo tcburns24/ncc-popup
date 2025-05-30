5b6c950d19e90384ef51fbc179b516049d1c2887
Thomas Burns <tcburns24@gmail.com>
2025-05-30 10:19:58 -0400

    Fixes duplicate POST submission bug

--- app/public/wp-content/themes/ncc-popup-child/ncc-script.js
+++ app/public/wp-content/themes/ncc-popup-child/ncc-script.js
@@ -1,21 +1,23 @@
 jQuery(document).ready(function ($) {
-  $("#ncc-contact-form").on("submit", function (e) {
-    e.preventDefault();
-    const formData = $(this).serialize();
+  $("#ncc-contact-form")
+    .off("submit")
+    .on("submit", function (e) {
+      e.preventDefault();
+      const formData = $(this).serialize();
 
-    $.post(
-      ncc_ajax_obj.ajax_url,
-      {
-        action: "ncc_submit_form",
-        data: formData,
-      },
-      function (response) {
-        $("#ncc-form-message").text(response.message).show();
-        if (response.success) {
-          $("#ncc-contact-form")[0].reset();
-        }
-      },
-      "json"
-    );
-  });
+      $.post(
+        ncc_ajax_obj.ajax_url,
+        {
+          action: "ncc_submit_form",
+          data: formData,
+        },
+        function (response) {
+          $("#ncc-form-message").text(response.message).show();
+          if (response.success) {
+            $("#ncc-contact-form")[0].reset();
+          }
+        },
+        "json"
+      );
+    });
 });
